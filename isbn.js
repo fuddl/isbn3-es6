47cd5dd0479a102c3d76cc29808209f559b42bb5
Alex <blank@bc1fb054-fc6c-44bf-b995-d928ab56e65c.fritz.box>
2024-05-15 08:31:40 +0200

    rewrite to es6

--- isbn.js
+++ isbn.js
@@ -1,28 +1,33 @@
-const parse = require('./lib/parse')
-const audit = require('./lib/audit')
+import parse from './lib/parse.js';
+import audit from './lib/audit.js';
+import groups from './lib/groups.js';
 
-module.exports = {
+const hyphenate = val => {
+  const data = parse(val);
+  if (!data) return null;
+  return data.isIsbn13 ? data.isbn13h : data.isbn10h;
+};
+
+const asIsbn13 = (val, hyphen) => {
+  const data = parse(val);
+  if (!data) return null;
+  return hyphen ? data.isbn13h : data.isbn13;
+};
+
+const asIsbn10 = (val, hyphen) => {
+  const data = parse(val);
+  if (!data) return null;
+  if (!data.isbn10) return null;
+  return hyphen ? data.isbn10h : data.isbn10;
+};
+
+const ISBN = {
   parse,
   audit,
+  hyphenate,
+  asIsbn13,
+  asIsbn10,
+  groups,
+};
 
-  hyphenate: val => {
-    const data = parse(val)
-    if (!data) return null
-    return data.isIsbn13 ? data.isbn13h : data.isbn10h
-  },
-  asIsbn13: (val, hyphen) => {
-    const data = parse(val)
-    if (!data) return null
-    return hyphen ? data.isbn13h : data.isbn13
-  },
-  asIsbn10: (val, hyphen) => {
-    const data = parse(val)
-    if (!data) return null
-    // Return null for cases where it shouldn't map to an ISBN 10
-    // ex: 979-10-91146-13-5
-    if (!data.isbn10) return null
-    return hyphen ? data.isbn10h : data.isbn10
-  },
-
-  groups: require('./lib/groups')
-}
+export default ISBN;
